3e8f08cbd8a19cd805d0f1d991f589cbcb2127f1
n-kubiak <nk00230@surrey.ac.uk>
2021-11-12 11:49:37 +0000

    Update args.py

--- tools/args.py
+++ tools/args.py
@@ -12,14 +12,14 @@ class BaseOptions():
         # meaningful params
         self.parser.add_argument('--single', action='store_true', help='flip to 1 to use only one folder (debug mode)')
         self.parser.add_argument('--num_imgs', type=int, default=10, help='how many imgs to use with MIT dataset')
-        self.parser.add_argument('--no_norm', type=int, default=1, help='normalize images?')
+        self.parser.add_argument('--no_norm', type=int, default=1, help='normalize images? (default: no)')
         self.parser.add_argument('--decomp_model', type=str, default='liu')
 
         # loss params
         self.parser.add_argument('--use_vgg_loss', type=int, default=1, help='use perceptual loss')
-        self.parser.add_argument('--use_os_loss', type=int, default=1, help='use Output Similarity loss, ie make outA&B similar')
+        self.parser.add_argument('--use_os_loss', type=int, default=1, help='use Output Similarity (OS) loss, ie make outA&B similar')
         self.parser.add_argument('--use_gradient_loss', type=int, default=1, help='spatial gradient loss, between outputs')
-        self.parser.add_argument('--use_reflectance_loss', type=int, default=1, help='compare shading between branches')
+        self.parser.add_argument('--use_reflectance_loss', type=int, default=1, help='compare reflectance between branches')
 
         # training params
         self.parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2')
@@ -44,14 +44,14 @@ class BaseOptions():
         self.parser.add_argument('--from_scratch', action='store_true', help='train from scratch and not from latest')
         self.parser.add_argument('--which', type=str, default='latest', help='which epoch to load (for ckpts)')
         self.parser.add_argument('--save_models', action='store_true', help='use flag to save models during training')
-        self.parser.add_argument('--checkpoints_dir', type=str, default='/vol/research/relighting/code/SILT/checkpoints/')
+        self.parser.add_argument('--checkpoints_dir', type=str, default='YOUR-PATH-TO-SILT/checkpoints/')
         self.parser.add_argument('--tb_log', action='store_true', help='if specified, use tensorboard logging')
-        self.parser.add_argument('--tb_dir', type=str, default='$YOUR-DIR')
+        self.parser.add_argument('--tb_dir', type=str, default='YOUR-PATH-TO-SILT/tb_runs/')
         self.parser.add_argument('--display_freq', type=int, default=1000, help='frequency of showing training results')
         self.parser.add_argument('--save_latest_freq', type=int, default=1000, help='frequency of saving the latest results')
         self.parser.add_argument('--save_epoch_freq', type=int, default=1, help='frequency of saving checkpoints at the end of epochs')
 
-        # for generator -> copied directly from pix2pix
+        # for generator
         self.parser.add_argument('--netG', type=str, default='global', help='selects model to use for netG')
         self.parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in first conv layer')
         self.parser.add_argument('--n_downsample_global', type=int, default=3, help='number of downsampling layers in netG - default: 4')
@@ -60,7 +60,7 @@ class BaseOptions():
         self.parser.add_argument('--n_local_enhancers', type=int, default=1, help='number of local enhancers to use')
         self.parser.add_argument('--niter_fix_global', type=int, default=0, help='number of epochs that we only train the outmost local enhancer')
 
-        # for discriminator -> copied directly from pix2pix
+        # for discriminator
         self.parser.add_argument('--num_D', type=int, default=2, help='number of discriminators to use')
         self.parser.add_argument('--n_layers_D', type=int, default=3, help='only used if which_model_netD==n_layers')
         self.parser.add_argument('--ndf', type=int, default=64, help='# of discrim filters in first conv layer')
